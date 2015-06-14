34906751add7ac4e4549edb92648e5034b97fae2
Priyadarshan <priyadarshan@vasudevaserver.org>
2015-06-14 15:29:49 +0000

    Initial commit

---
+++ news/xmlrpc0db0.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>http://wordpress.org/</engineLink>
+    <homePageLink>http://www.srichinmoybio.co.uk/news</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://www.srichinmoybio.co.uk/news/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://www.srichinmoybio.co.uk/news/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://www.srichinmoybio.co.uk/news/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://www.srichinmoybio.co.uk/news/xmlrpc.php" />
+          </apis>
+  </service>
+</rsd>
